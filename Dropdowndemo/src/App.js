7d2d30a2e3d5f962eaee684f86a10c4326fa054e
Mayurigore12 <80272927+Mayurigore12@users.noreply.github.com>
2021-04-13 08:28:12 +0530

    Add files via upload

---
+++ Dropdowndemo/src/App.js
@@ -0,0 +1,14 @@
+import react, { Component } from 'react';
+import Dropdown from './components/Dropdown'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      <Dropdown/>
+      </div>
+    );
+  }
+}
+
+export default App;
